79e8931dbcd5c10a44f4af4e271c405477ff115a
ani213 <vermaaniket213@gmail.com>
2025-02-04 20:27:23 +0530

    auth router created

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	"github.com/ani213/go-jwt-project/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("user/signup", controllers.Signup())
+	incomingRoutes.POST("user/login", controllers.Login())
+}
