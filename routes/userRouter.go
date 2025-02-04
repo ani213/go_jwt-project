a3e4bbaa5a3c5050210fa7c0e516e9cf40de419d
ani213 <vermaaniket213@gmail.com>
2025-02-04 20:27:57 +0530

    routes created

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/ani213/go-jwt-project/controllers"
+	"github.com/ani213/go-jwt-project/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	// incomingRoutes.GET("/user", controllers.GetUsers())
+	incomingRoutes.GET("/user/:user_id", controllers.GetUser())
+
+}
